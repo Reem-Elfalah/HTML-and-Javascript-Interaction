d01e21b063d6d9fd3bc540d1f2038c203922cc6a
oussema-sferi <oussema.sferi@gmail.com>
2020-04-11 15:10:43 +0100

    all solved

--- webDevelopment_part2/webDevelopment.js
+++ webDevelopment_part2/webDevelopment.js
@@ -9,7 +9,7 @@ alert(this.name + " says hi");
 };
 var bob = { name: "Bob" };
 
-setTimeout(alice.sayHi.call(bob), 1000);
+//setTimeout(alice.sayHi.call(bob), 1000);
 
 // Answer Choices:
 // Window says hi, after 1 second
@@ -26,7 +26,7 @@ function sayHello () {
 alert(this.name + " says hello!");
 }
 var alice = { name: "Alice" };
-sayHello.call(alice);
+//sayHello.call(alice);
 // 3-
 // After running the following code, what will be the value of result?
 
@@ -53,13 +53,14 @@ var bob = {
 name: 'Bob',
 greet: alice.greet
 }
-var a = bob.greet()
+
+
 var greet = alice.greet
 var result = greet()
 // Answer Choices:
 // "Hi I am Alice"
 // "Hi I am Bob"
-// "Hi I am window"
+// "Hi I am window" ==>correct Answer
 // "Hi I am undefined"
 // 5-
 // Fill the following survey INDIVIDULLY, you can read the passage (in the survey) in pairs, but make 
@@ -69,35 +70,35 @@ var result = greet()
 // 6-
 // After running the following code, what will be the value of result?
 
-// var obj1 = { x: 10 };
+var obj1 = { x: 10 };
 
-// var obj2 = Object.create(obj1);
+var obj2 = Object.create(obj1);
 
-// var obj3 = Object.create(obj2);
+var obj3 = Object.create(obj2);
 
-// var result = obj3.x + 10;
+var result = obj3.x + 10;
 // Answer Choices:
 // 10
-// 20
+// 20 ==> correct
 // 30
 // NaN
 // 7-
 // What message will eventually be alerted? After how long?
 
-// var name = "Window";
-// var alice = {
-// name: "Alice",
-// sayHi: function() {
-// alert(this.name + " says hi");
-// }
-// };
-// var bob = { name: "Bob" };
+var name = "Window";
+var alice = {
+name: "Alice",
+sayHi: function() {
+alert(this.name + " says hi");
+}
+};
+var bob = { name: "Bob" };
 
-// setTimeout(alice.sayHi.bind(alice), 1000);
+setTimeout(alice.sayHi.bind(alice), 1000);
 // Answer Choices:
 // Window says hi, after 1 second
 // Window says hi, immediately
-// Alice says hi, after 1 second
+// Alice says hi, after 1 second ==> correct
 // Alice says hi, immediately
 // Window says hi, after 1 second
 // Bob says hi, immediately
@@ -108,4 +109,4 @@ var result = greet()
 // modularize our code for reuse and composition
 // centralize code to simplify maintenance
 // reduce errors and repetition
-// DRY - don't repeat yourself (developer efficacy)
+// DRY - don't repeat yourself (developer efficacy) ==>correct
