1946689b9e46855cff5bbfb7e4a224e0f72194d0
Reem-Elfalah <r.fellah@yahoo.com>
2020-04-11 14:37:37 +0100

    1,2,3 solved

--- webDevelopment_part2/webDevelopment.js
+++ webDevelopment_part2/webDevelopment.js
@@ -1,66 +1,69 @@
 // 1-
 // What message will eventually be alerted? After how long?
-// var name = "Window";
-// var alice = {
-// name: "Alice",
-// sayHi: function() {
-// alert(this.name + " says hi");
-// }
-// };
-// var bob = { name: "Bob" };
+var name = "Window";
+var alice = {
+name: "Alice",
+sayHi: function() {
+alert(this.name + " says hi");
+}
+};
+var bob = { name: "Bob" };
 
-// setTimeout(alice.sayHi.call(bob), 1000);
+setTimeout(alice.sayHi.call(bob), 1000);
+
 // Answer Choices:
 // Window says hi, after 1 second
 // Window says hi, immediately
 // Alice says hi, after 1 second
 // Alice says hi, immediately
 // Bob says hi, after 1 second
-// Bob says hi, immediately
+// Bob says hi, immediately  [correct]
 // 2-
-// Write a line of code that uses .call (from Function.prototype.call) to alert the string "Alice says hello!"
+// Write a line of code that uses .call (from Function.prototype.call) to alert the string 
+//"Alice says hello!"
 
-// function sayHello () {
-// alert(this.name + " says hello!");
-// }
-// var alice = { name: "Alice" };
-// // ???
+function sayHello () {
+alert(this.name + " says hello!");
+}
+var alice = { name: "Alice" };
+sayHello.call(alice);
 // 3-
 // After running the following code, what will be the value of result?
 
-// var x = 10;
-// var puzzle = function (amount) {
-// this.x += amount;
-// return this.x;
-// };
+var x = 10;
+var puzzle = function (amount) {
+this.x = this.x + amount;
+return this.x;
+};
 
-// var result = 50 - puzzle(20);
+var result = 50 - puzzle(20);
 // 4-
 // After running the following code, what will be the value of result?
 
-// window.name = 'window'
+window.name = 'window'
 
-// var alice = {
-// name: 'Alice',
-// greet: function () {
-// return "Hi I am " + this.name
-// }
-// }
+var alice = {
+name: 'Alice',
+greet: function () {
+return "Hi I am " + this.name
+}
+}
 
-// var bob = {
-// name: 'Bob',
-// greet: alice.greet
-// }
-
-// var greet = alice.greet
-// var result = greet()
+var bob = {
+name: 'Bob',
+greet: alice.greet
+}
+var a = bob.greet()
+var greet = alice.greet
+var result = greet()
 // Answer Choices:
 // "Hi I am Alice"
 // "Hi I am Bob"
 // "Hi I am window"
 // "Hi I am undefined"
 // 5-
-// Fill the following survey INDIVIDULLY, you can read the passage (in the survey) in pairs, but make sure you fill the survey individully, we trust that each one will answer on his own
+// Fill the following survey INDIVIDULLY, you can read the passage (in the survey) in pairs, but make 
+//sure you fill the survey individully, we trust that each one will answer on his own
 // HERE
 
 // 6-
